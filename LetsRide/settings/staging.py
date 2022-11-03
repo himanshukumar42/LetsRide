4c36aa18081bfa5c37e00c6826338e0d8f307956
Himanshu Kumar <kumarhimanshu250798@gmail.com>
2022-11-03 17:39:46 +0530

    Added Multiple environments for the Project

---
+++ LetsRide/settings/staging.py
@@ -0,0 +1,19 @@
+from LetsRide.settings.common import *
+from dotenv import find_dotenv, load_dotenv
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'staging-secret')
+DEBUG = False
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
+    }
+}
